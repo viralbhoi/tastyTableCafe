28fadd5d498f2d5de32f46e116f083f682d45818
Viral Bhoi <141637546+viralbhoi@users.noreply.github.com>
2024-06-28 17:01:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,29 +8,3 @@ gsap.to("#main",{
         scrub:2,
     }
 })
-
-gsap.from(".b1", {
-    scale:0.75,
-    opacity:0,
-    duration:1,
-    scrollTrigger:{
-        trigger:".b1",
-        scroller:"body",
-        start:"top 70%",
-        end:"top 65%",
-        scrub:1
-    }
-})
-
-
-gsap.from("#page4 h2",{
-    y:-50,
-    scrollTrigger:{
-        trigger:"#page4 h2",
-        scroller:"body",
-        // markers:true,
-        start:"top 75%",
-        end:"top 70%",
-        scrub:2,
-    }
-})
